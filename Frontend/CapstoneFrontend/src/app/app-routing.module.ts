c24b267ceffbc0926166124ce0d8fb285391af9d
jimboden1 <jimboden1@murraystate.edu>
2021-09-14 13:24:45 -0400

    created login and sign up in front end

--- Frontend/CapstoneFrontend/src/app/app-routing.module.ts
+++ Frontend/CapstoneFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { SignupComponent } from './signup/signup.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [{path:"", component:MainComponent},
-{path:"user",component:UserComponent}];
+{path:"user",component:UserComponent},
+{path:"login",component:LoginComponent},
+{path:"signup",component:SignupComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/CapstoneFrontend/src/app/app.module.ts
+++ Frontend/CapstoneFrontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    UserComponent
+    UserComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
